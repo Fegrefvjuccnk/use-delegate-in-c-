1c3925efe29e3502da6734fff5458fd5a155b85c
ebrahem elgharib <153863368+Fegrefvjuccnk@users.noreply.github.com>
2024-10-16 21:26:19 +0300

    Add files via upload

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace ConsoleApp9
+{
+
+  //public delegate int stringfuncdelegate(string name);
+    internal class Program
+    {
+        //public static int getcountofupperstrfunc(string name) {
+        //    int count = 0;
+
+
+        //        if(name != null)
+        //        for(int i = 0; i < name.Length; i++)
+        //        {
+        //            if (char.IsUpper(name[i]))
+        //            {
+        //                count++;
+        //            }
+        //        }
+        //    return count;
+        
+        //}
+        //public static int getcountoflowerstrfunc(string name)
+        //{
+        //    int count = 0;
+
+
+        //    if (name != null)
+        //        for (int i = 0; i < name.Length; i++)
+        //        {
+        //            if (char.IsLower(name[i]))
+        //            {
+        //                count++;
+        //            }
+        //        }
+        //    return count;
+
+        //}
+
+        static void Main(string[] args)
+        {
+
+            //string[] name = { "ebrahem", "mohamed", "ali", "khaled", "mabrouk" };
+            //bubblesort<String> .bubllsort(name, comparefuncton.funcgrtstring);
+            //foreach (string names in name)
+            //{
+            //    Console.WriteLine(names);
+            //}
+
+            //int[] number = { 1, 20, 4, 6, 7, 0, 9, 2, 5 };
+
+            //bubblesort.bubllsort(number, comparefuncton.funcless);
+
+            //foreach ( int numbers in number)
+            //{
+            //    Console.WriteLine(numbers);
+            //}
+            //stringfuncdelegate stringfunc;
+            //stringfunc = getcountofupperstrfunc;
+            //stringfunc +=getcountoflowerstrfunc;
+
+            //int result = stringfunc.Invoke("ebrahem");
+
+            //Console.WriteLine(result);
+            // List<int> number = Enumerable.Range(0, 100).ToList();
+            //List<int>oddnumber=bubblesort.getelmentbyallpramters(number,logicfunction.elmenteven, result);
+            // foreach (int num in oddnumber) {
+            //     Console.Write(num+" ");
+            // }
+            List<string> name = new List<string>() { "ebrahem", "ali", "mona", "ahmed", "aya", "sayed " };
+
+            List<string> nameoflengthfour = bubblesort.getelmentbyallpramters(name, logicfunction.getelmentlfour);
+            foreach (string names in nameoflengthfour)
+            {
+                Console.WriteLine(names);
+            }
+
+
+        }
+    }
+}
---
+++ ConsoleApp9/bubblesort.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    //public delegate bool sortfunc<T1>(T1 a, T1 b);
+    public delegate bool logicfunc<in T>(T x);
+    class bubblesort/*<T>*/
+    {
+
+        //public static void bubllsort( T[] array,sortfunc<T> sortfunc)
+        //{
+        //    if(array != null)
+        //    {
+        //        for(int i = 0; i < array.Length; i++)
+        //        {
+        //            for (int j = 0;j < array.Length-i-1; j++)
+        //            {
+        //                if (sortfunc(array[j], array[j + 1]))
+        //                swap( ref array[j], ref array[j+1]);
+        //            }
+        //        }
+        //    }
+        //}
+
+        //public static void swap <T2>(ref T2 x,ref T2 y)
+        //{
+        //   T2 temp=x;
+        //    x=y;
+        //    y=temp;
+        //}
+        public static List<T> getelmentbyallpramters<T>( List<T> elment,logicfunc<T> logicfunc)
+        {
+            List<T> result = new List<T>();
+            if(elment != null)
+            {
+                for(int i = 0; i < elment.Count; i++)
+                {
+                    if (logicfunc(elment[i])==true)
+                    result.Add(elment[i]);
+                }
+            }
+
+            return result;
+        }
+
+    }
+    //    class comparefuncton
+    //    {
+    //        public static bool funcgrt( int x, int y)
+    //        {
+    //            return x < y;
+    //        }
+    //        public static bool funcless( int x,  int y)
+    //        {
+    //            return x > y;
+    //        }
+    //        public static bool funcgrtstring(string x, string y)
+    //        {
+    //            return x?.Length < y?.Length;
+    //        }
+    //        public static bool funclessstring(string x, string y)
+    //        {
+    //            return x?.Length > y?.Length;
+    //        }
+    //    }
+
+    class logicfunction
+    {
+        public static bool elmentodd(int number) { return number % 2 == 0; }
+        public static bool elmenteven(int number) { return number % 2 == 1; }
+        public static bool getelmentlfour(string name) { return name.Length > 4; }
+    }
+}
